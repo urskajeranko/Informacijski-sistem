6ad2b5eab0bddbfddf2652d844a1cf1db2a45d66
Urška Jeranko <urska.jeranko@gmail.com>
2017-05-15 15:01:50 +0200

    Zastavila sem si funkcije

--- python/informacijski-sistem-funkcije.py
+++ python/informacijski-sistem-funkcije.py
@@ -1,4 +1,11 @@
-Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> #TU BODO FUNKCIJE ZA MOJ INFORMACIJSKI SISTEM
->>> 
+#TU BODO FUNKCIJE ZA MOJ INFORMACIJSKI SISTEM
+
+def narocilo(hrana, kolicina):
+
+def dnevna_poraba(hrana, kolicina):
+
+def povprecna_poraba(hrana, kolicina, dan):
+
+def zaloga(hrana, kolicina):
+
+def opozori(hrana, kolicina):
